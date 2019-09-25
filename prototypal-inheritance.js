cb3d6459f48bea619063f68b454893a22b83ccd7
oscarmtzo <icehman@hotmail.com>
2019-09-25 09:26:40 -0500

    literal object

--- prototypal-inheritance.js
+++ prototypal-inheritance.js
@@ -1,1 +1,67 @@
-let someFunction = () =>{}
+//constructor functions
+function person (name, lastName) {
+    this.name = name
+    this.lastName = lastName
+    // this.isMyFamily = function() {
+    //     return `this is my last name ${lastName}`
+    // }
+    this.isMyFamily = () => `this is my last name ${lastName}`
+}
+let myAunt = new person('Martha', 'de la O')
+console.log(myAunt.name)
+let granpa = new person('Angel', 'de la O')
+console.log(granpa, 'este es el segundo objeto con sus propiedades')
+granpa.getSon = () => `this is my daughter ${myAunt}` //-->
+// granpa.getSon = function(){ return `this is my daughter ${myAunt}`}
+
+let divAunt = document.getElementsByClassName('aunt')
+console.log(typeof divAunt,divAunt)
+divAunt[0].innerHTML = myAunt.name
+
+divAunt[0].style.backgroundColor = 'red'
+divAunt[0].style.width = 'max-content'
+// aconstructor function lets you creat or initialize objects using the same structure
+// --> is use to create instances of an object
+
+
+
+// creating objects with object literal notation:
+let obj = {
+    name: 'Oscar',
+    lastName: 'de la O'
+}
+console.log(obj)
+let div1 = document.createElement('div')
+div1.className = 'objName'
+let theObjNameDiv = document.getElementsByClassName('objName')
+theObjNameDiv.innerHTML = obj.name
+let holaDiv = document.getElementsByClassName('hola')
+holaDiv[0].innerHTML = 'hola'
+
+//create a method 'calculatePrice' for the constructure 'Item'
+function item (name, price ){
+    this.name = name
+    this.price = price
+    this.calculatePrice = function(){
+        let helper 
+        if(this.name == 'fruit'){
+            this.price= this.price - this.price*0.15
+            console.log(helper)
+            return `this is the real price ${this.price}`
+        }else{
+            return `this is the real price ${this.price}`
+        }
+    }
+}
+// item.prototype.calculatePrice = function(){
+//     let helper
+//     if(this.name == 'fruit'){
+//         helper =this.price - this.price*0.15
+//         return `this is the real price ${helper}`
+//     }else{
+//         return `this is the real price ${this.price}`
+//     }
+// }
+
+let ball = new item('ball', 15)
+let apple = new item('fruit', 10)
